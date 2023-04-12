224e10fe72d6019aef5b29a060c940df7193976b
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:20:22 +0300

    Add files via upload

---
+++ lab9/composer/composer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace composer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var jewelryBox = new JewelryBox { Name = "Велосипед" };
+            var topTray = new Tray { Name = "Деталі" };
+            var ringCompartment = new Compartment { Name = "------------" };
+
+            ringCompartment.AddContent("Колесо");
+            ringCompartment.AddContent("Рама");
+            ringCompartment.AddContent("Дзвінок");
+            topTray.Add(ringCompartment);
+            jewelryBox.Add(topTray);
+            jewelryBox.Display(0, textBox1);
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var jewelryBox = new JewelryBox { Name = "" };
+            var topTray = new Tray { Name = "" };
+            var ringCompartment = new Compartment { Name = "---Новий деталь---" };
+            ringCompartment.AddContent("Тормоза");
+            topTray.Add(ringCompartment);
+            jewelryBox.Add(ringCompartment);
+            jewelryBox.Display(0, textBox1);
+        }
+    }
+}
---
+++ lab9/composer/composer/composer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace composer
+{
+    public abstract class JewelryBoxComponent
+    {
+        public string Name { get; set; }
+        public abstract void Add(JewelryBoxComponent component);
+        public abstract void Remove(JewelryBoxComponent component);
+        public abstract void Display(int depth, TextBox textBox);
+    }
+
+    public class JewelryBox : JewelryBoxComponent
+    {
+        private List<JewelryBoxComponent> coomponents = new List<JewelryBoxComponent>();
+        public override void Add(JewelryBoxComponent component)
+        {
+            coomponents.Add(component);
+        }
+        public override void Display(int depth, TextBox textBox)
+        {
+            textBox.AppendText(new string('-', depth) + " " + Name + Environment.NewLine);
+            foreach (var component in coomponents)
+            {
+                component.Display(depth + 2, textBox);
+            }
+        }
+        public override void Remove(JewelryBoxComponent component)
+        {
+            coomponents.Remove(component);
+        }
+    }
+    public class Tray : JewelryBoxComponent
+    {
+        private List<JewelryBoxComponent> coomponents = new List<JewelryBoxComponent>();
+        public override void Add(JewelryBoxComponent component)
+        {
+            coomponents.Add(component);
+        }
+        public override void Display(int depth, TextBox textBox)
+        {
+            textBox.AppendText(new string('-', depth) + " " + Name + Environment.NewLine);
+            foreach (var component in coomponents)
+            {
+                component.Display(depth + 2, textBox);
+            }
+        }
+        public override void Remove(JewelryBoxComponent component)
+        {
+            coomponents.Remove(component);
+        }
+    }
+    public class Compartment : JewelryBoxComponent
+    {
+        private List<string> contents = new List<string>();
+        public void AddContent(string item)
+        {
+            contents.Add(item);
+        }
+        public override void Add(JewelryBoxComponent component)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Display(int depth, TextBox textBox)
+        {
+            textBox.AppendText(new string('-', depth) + " " + Name + Environment.NewLine);
+            foreach (var item in contents)
+            {
+                textBox.AppendText(new string('-', depth + 2) + " " + item + Environment.NewLine);
+            }
+        }
+        public override void Remove(JewelryBoxComponent component)
+        {
+            throw new NotImplementedException();
+            //coomponents.Add(component);
+        }
+    }
+}
+
+
+
+
+
+
